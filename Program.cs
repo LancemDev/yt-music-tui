751bca2d0942a98b9c2ab3ac1658f574221b6b44
LancemDev <munyaolance1@gmail.com>
2026-08-14 15:00:16 +0300

    :zap: setup: Project initialization and ratatuics setup

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Ratatui;
+
+using var term = new Terminal();
+term.Clear();
+
+using var para = new Paragraph("Hello from Ratatui.cs!")
+    .Title("YT Music TUI");
+
+term.Draw(para, new Rect(2, 1, 44, 6));
